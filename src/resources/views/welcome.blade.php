4b1e7868ca08ba28692d7b9eb74b5e9f85498920
mizushinaruka <mizushina_ruka@giginc.co.jp>
2025-09-15 22:46:59 +0900

    追加したら一覧に出るようにした

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -6,61 +6,68 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<script>
-    //一覧取得
-    async function loadTodos() {
-        const res = await fetch('/lists', { headers: { 'Accept': 'application/json' } });
-        if (!res.ok) throw new Error('HTTP ' + res.status);
-        return await res.json();
-    }
-
-    //todoを<li>に入れる
-    function createTodos(todos) {
-        const ul = document.getElementById('todo-list');
-        ul.innerHTML = '';
-        // 1件ずつ <li> を作って追加
-        todos.forEach(todo => {
-        const li = document.createElement('li');
-        // チェックボックスを作る
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.checked = todo.status;
-        // タイトルを作る
-        const span = document.createElement('span');
-        li.textContent = todo.title;
-        // 削除ボタン
-        const button = document.createElement('button');
-        button.textContent = '削除';
-        // li に追加していく
-        li.appendChild(checkbox);
-        li.appendChild(span);
-        li.appendChild(button);
-        ul.appendChild(li);
-        });
-   }
-
-   //画面読み終わったら実行する
-   document.addEventListener('DOMContentLoaded', async () => {
-        const todos = await loadTodos();
-        createTodos(todos);
-
-        // フォームが送信されたら一覧を再読み込みする
-        const form = document.getElementById('create-form');
-        form.addEventListener('submit', async () => {
-        const todos = await loadTodos();
-        createTodos(todos);
-        });
-    });
-
-</script>
 <body>
-    <form id="create-form" action="/create" method="POST">
-        <input name="title">
+    <form id="todo-form" action="/create" method="POST">
+        <input name="title" id="title">
         <input type="submit">
     </form>
     <ul id="todo-list"></ul>
 
-        <input type="checkbox" name="status">
-        </input>
+    <script>
+        //一覧取得
+        async function loadTodos() {
+            const res = await fetch('/lists', { headers: { 'Accept': 'application/json' } });
+            if (!res.ok) throw new Error('HTTP ' + res.status);
+            return await res.json();
+        }
+
+        //一覧表示
+        function createTodos(todos) {
+            const ul = document.getElementById('todo-list');
+            ul.innerHTML = '';
+            // 1件ずつ <li> を作って追加
+            todos.forEach(todo => {
+            const li = document.createElement('li');
+            // チェックボックスを作る
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.checked = todo.status;
+            // タイトルを作る
+            const span = document.createElement('span');
+            span.textContent = todo.title;
+            // 削除ボタン
+            const button = document.createElement('button');
+            button.textContent = '削除';
+            // li に追加していく
+            li.appendChild(checkbox);
+            li.appendChild(span);
+            li.appendChild(button);
+            ul.appendChild(li);
+            });
+    }
+
+        // 初回表示
+        (async () => {
+            const todos = await loadTodos();
+            createTodos(todos);
+        })();
+
+    // フォーム送信
+        document.getElementById('todo-form').addEventListener('submit', async (e) => {
+        e.preventDefault(); 
+
+        const title = document.getElementById('title').value;
+
+        await fetch('/create', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: new URLSearchParams({ title })
+        });
+
+        const todos = await loadTodos();
+        createTodos(todos);
+        document.getElementById('title').value = '';
+        });
+    </script>
 </body>
 </html>
