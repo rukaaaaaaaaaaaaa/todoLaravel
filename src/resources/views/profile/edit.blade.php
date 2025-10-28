3978cbd96a18685f6fccc8385ce776dc3877510a
mizushinaruka <mizushina_ruka@giginc.co.jp>
2025-10-28 16:33:02 +0900

    デザイン少し調整した

--- src/resources/views/profile/edit.blade.php
+++ src/resources/views/profile/edit.blade.php
@@ -26,6 +26,10 @@
                         
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
+                    <header>
+                        <h2 class="text-lg font-medium text-gray-900">Profile Pic</h2>
+                        <p class="mt-1 text-sm text-gray-600">Upload your picture.</p>
+                    </header>
                     @if ($user->avatar_path)
                         <img src="{{ config('filesystems.disks.s3.url') }}/{{ config('filesystems.disks.s3.bucket') }}/{{ $user->avatar_path }}"
                         alt="プロフィール画像" width="300">
@@ -36,8 +40,19 @@
                     <form method="POST" action="/profile/upload" enctype="multipart/form-data">
                         @csrf
                         <input type="file" name="avatar">
-                        <button type="submit">アップロード</button>
+                        <br>
+                        <br>
+                        <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">アップロード</button>
                     </form>
+                </div>
+            </div>
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    <header>
+                        <h2 class="text-lg font-medium text-gray-900">About Me</h2>
+                        <p class="mt-1 text-sm text-gray-600">Text text.</p>
+                    </header>
                 </div>
             </div>
 
